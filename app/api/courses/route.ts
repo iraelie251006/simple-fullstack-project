00063ed36478b85887b548467002b70bee4c1ae2
Elie <niyubwayoiraelie5777@gmail.com>
2024-11-15 21:13:02 +0200

    add my simple fullstack project

---
+++ app/api/courses/route.ts
@@ -0,0 +1,23 @@
+// /api/courses/route.ts
+
+import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
+
+export async function POST (
+    req: Request
+) {
+    try {
+        const { title } = await req.json()
+        const course = await db.courses.create({
+            data: {
+                title
+            }
+        })
+
+        return NextResponse.json(course)
+
+    } catch (error) {
+        console.log("COURSE_ERROR", error)
+        return new Response("An error occurred", { status: 500 })
+    }
+}
